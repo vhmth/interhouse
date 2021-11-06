75c1b67ad4f58ccaef1d12a9224a66d8e7231218
Vinay Hiremath <vhiremath4@gmail.com>
2021-11-06 12:40:31 -0400

    next-ify app and add operating manual

---
+++ pages/index.js
@@ -0,0 +1,119 @@
+import Head from 'next/head'
+
+export default function() {
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" name="viewport" content="user-scalable=no, width=device-width" />
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css' />
+        <link rel="stylesheet" type="text/css" href="styles.css" />
+      </Head>
+
+      <body>
+        <h1>vinay</h1>
+        <p>about me:</p>
+        <ul>
+        <li>i am the co-founder and cto at <a target="_blank" href="https://www.loom.com">loom</a>.
+          <ul>
+            <li>$1.5b company.</li>
+            <li>$200m raised in funding.</li>
+            <li>employs 200 talented people from new zealand to poland.</li>
+            <li>used by over 15m people worldwide.</li>
+          </ul>
+        </li>
+        <li>i invest in companies and am willing to offer help to founders i vibe with for free and for no allocation.</li>
+        <li>i care about animal welfare and the environment, and i believe lab-grown meat is the solution to most of these problems. i invest in lab-grown meat companies, have a background in materials science, and am currently self-studying biochemistry. please reach out if you're working on something novel in this space - i would love to be helpful even if there isn't an investment opportunity.</li>
+        <li>i regularly take psychedelics and do cold water/sauna therapy.</li>
+        <li>i am a <a target="_blank" href="https://www.amazon.com/Conscious-Capitalism-New-Preface-Authors/dp/1625271751">conscious capitalist</a>.</li>
+        <li>adventure and movement makes me happy. i like throwing parties, riding motorcycles, rock climbing, lifting weights, and am usually down to try anything once.</li>
+        </ul>
+        <p>social:</p>
+        <ul>
+          <li><a target="_blank" href="https://twitter.com/vhmth">twitter</a> - avoid telling yourself "i am right and you are wrong"</li>
+          <li><a target="_blank" href="https://instagram.com/vhmth">instagram</a> - avoid telling yourself "i am attractive and you are not"</li>
+          <li><a target="_blank" href="https://facebook.com/vhmth">facebook</a> - avoid telling yourself "i am fulfilled and you are not"</li>
+          <li><a target="_blank" href="https://linkedin.com/in/vhiremath4">linkedin</a> - avoid telling yourself "i am accomplished and you are not"</li>
+        </ul>
+        <p>operating manual:</p>
+        <ul>
+          <li><a target="_blank" href="https://www.notion.so/vinay/If-you-want-to-work-with-me-at-Loom-d54167b7090a49b9b53d653815daa0da">if you want to work with me at loom</a></li>
+        </ul>
+        <p>tings:</p>
+        <ul>
+          <li><a target="_blank" href="https://www.forbes.com/video/6268512444001/how-this-153-billion-video-company-is-revolutionizing-remote-work">forbes future of work video with joe</a></li>
+          <li><a target="_blank" href="https://venturebeat.com/2021/06/02/looms-new-sdk-lets-any-company-add-video-messaging-to-their-apps/">loom sdk beta venturebeat exclusive</a></li>
+          <li><a target="_blank" href="https://www.loom.com/blog/loom-series-c">$130m series c (loom)</a></li>
+          <li><a target="_blank" href="https://www.youtube.com/watch?v=SCKWmJ3gU_c">fireside chat with sfelc</a></li>
+          <li><a target="_blank" href="https://www.forbes.com/sites/alexandrawilson1/2020/05/28/exclusive-why-all-star-investors-think-350-million-video-startup-loom-is-the-next-big-thing-in-remote-work-tech">$350m valuation series b+ (loom)</a></li>
+          <li><a target="_blank" href="https://www.notion.so/customers/loom">notion customer testimony</a></li>
+          <li><a target="_blank" href="https://techcrunch.com/2019/11/26/loom-enterprise-video">$30m series b (loom)</a></li>
+          <li>u.s. patent no. <a target="_blank" href="http://patft.uspto.gov/netacgi/nph-Parser?Sect1=PTO1&Sect2=HITOFF&d=PALL&p=1&u=%2Fnetahtml%2FPTO%2Fsrchnum.htm&r=1&f=G&l=50&s1=10,484,737.PN.&OS=PN/10,484,737&RS=PN/10,484,737">10,484,737</a> - video and audio creation on steroids, more loosely defined</li>
+          <li><a target="_blank" href="https://podcasts.apple.com/us/podcast/does-a-chip-on-your-shoulder-help-you-become-successful/id1354589767?i=1000434944095">podcast on having a chip on your shoulder</a></li>
+          <li><a target="_blank" href="https://techcrunch.com/2019/02/19/loom-video/">$11m series a (loom)</a></li>
+          <li><a target="_blank" href="https://www.forbes.com/sites/alexandrawilson1/2018/06/07/why-slack-and-general-catalyst-just-backed-video-platform-loom-in-4-million-round/#45ed9a8378f7">$4m seed (loom)</a></li>
+          <li><a target="_blank" href="http://www.nwherald.com/2017/12/26/huntley-high-school-alumnus-named-to-forbes-30-under-30-list/ayft2o0/">local paper</a></li>
+          <li><a target="_blank" href="https://www.forbes.com/profile/loom/?list=30under30-enterprise-technology">forbes 30 under 30</a></li>
+          <li><a target="_blank" href="https://github.com/videojs/video.js/pull/4591">buttery videos for the internet</a></li>
+          <li>u.s. patent no. <a target="_blank" href="http://patft1.uspto.gov/netacgi/nph-Parser?patentnumber=9641566">9,641,566</a> - video and audio creation on steroids</li>
+          <li><a target="_blank" href="https://vimeo.com/194083769">patent attorney testimonial</a></li>
+          <li><a target="_blank" href="https://www.loom.com">loom</a></li>
+        </ul>
+        <div style={ {
+          position: 'relative',
+          paddingBottom: '52.785923753665685%',
+          height: 0
+        } } >
+          <iframe loading="lazy" src="https://www.loom.com/embed/5815b026ce064248909a3ec4be0d82a3" frameBorder="0" allowFullScreen style={ {
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%'
+          } }></iframe>
+        </div>
+        <br />
+        <div style={ {
+          position: 'relative',
+          paddingBottom: '51.77372962607861%',
+          height: 0
+        } } >
+          <iframe loading="lazy" src="https://www.loom.com/embed/4da94e0fd8654cc183bd5760c07eecc2" frameBorder="0" allowFullScreen style={ {
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%'
+          } }></iframe>
+        </div>
+        <br />
+        <div style={ {
+          position: 'relative',
+          paddingBottom: '62.5%',
+          height: 0
+        } } >
+          <iframe loading="lazy" src="https://www.loom.com/embed/70ef16bf3e044389815e539685edd401" frameBorder="0" allowFullScreen style={ {
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%'
+          } }></iframe>
+        </div>
+        <br/>
+        <div style={ {
+          position: 'relative',
+          paddingBottom: '62.5%',
+          height: 0
+        } } >
+          <iframe loading="lazy" src="https://www.loom.com/embed/df31eb35d10b49abbc5323f503a7afec" frameBorder="0" allowFullScreen style={ {
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%'
+          } }></iframe>
+        </div>
+      </body>
+    </>
+  );
+}
